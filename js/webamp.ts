830fede55dd01d04a62abd8a3c74e3c87189f3ad
bilaldemir <18368833+bilalvdemir@users.noreply.github.com>
2025-05-29 11:52:28 +0300

    first commit

---
+++ js/webamp.ts
@@ -0,0 +1,20 @@
+import JSZip from "jszip";
+import * as musicMetadataBrowser from "music-metadata-browser";
+import { Options } from "./types";
+import WebampLazy, { PrivateOptions } from "./webampLazy";
+
+export type * from "./types";
+
+export default class Webamp extends WebampLazy {
+  constructor(options: Options & PrivateOptions) {
+    super({
+      ...options,
+      requireJSZip: async () => JSZip,
+      requireMusicMetadata: async () => musicMetadataBrowser,
+    });
+  }
+}
+
+// Bit of a hack here. This overwrites the value set in WebampLazy.ts
+// @ts-ignore
+window.Webamp = Webamp;
